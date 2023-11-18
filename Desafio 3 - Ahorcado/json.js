f0c491f046a782fd758b5d67c510ac2fc0411d5c
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2023-11-18 00:17:53 -0300

    Pa seguir

--- Desafio 3 - Ahorcado/json.js
+++ Desafio 3 - Ahorcado/json.js
@@ -1,56 +1,91 @@
+
 let img = document.querySelector('.ahorcado');
 let rayaContainer = document.querySelector('.rayas');
 let pregunta = document.querySelector('b');
 let btns = document.querySelectorAll('.btn');
 
-function botones(){
-    btns.forEach(btn => {
-        btn.addEventListener("click", () => {
-            btn.disabled = true;
-        });
-    });
-}
-
+let error = document.querySelector('.error');
 
 const preguntas = [
-    { pregunta: "¿Cuál es la capital de Francia?", respuesta: "París" },
+    { pregunta: "¿Cuál es la capital de Francia?", respuesta: "Paris" },
     { pregunta: "¿Cuál es el río más largo del mundo?", respuesta: "Amazonas" },
     { pregunta: "¿Cuál es el peor champ?", respuesta: "Teemo" },
     { pregunta: "¿Cuál es el champ mas crack?", respuesta: "Bardo" },
 ];
 
-function generarPreguntas(){
-    let random = Math.floor(Math.random() * preguntas.length);
+const imagenes = [
+    img.src = 'media/1.svg',
+    img.src = 'media/2.svg',
+    img.src = 'media/3.svg',
+    img.src = 'media/4.svg',
+    img.src = 'media/5.svg',
+    img.src = 'media/6.svg',
+]
 
-    let pregRandom = preguntas[random];
-    let rtaRandom = pregRandom.respuesta;
 
-    pregunta.innerHTML = pregRandom.pregunta;
-    
-    let letras = obtenerLetras(rtaRandom);
-    for (let i = 0; i < rtaRandom.length ; i++) {
-        let rayaLi = document.createElement('li');
-        
-        rayaLi.textContent = letras[i];
-        rayaContainer.append(rayaLi);
-    }
+
+let random = Math.floor(Math.random() * preguntas.length);
+let pregRandom = preguntas[random];
+let rtaRandom = pregRandom.respuesta.toUpperCase();
+console.log(rtaRandom);
+
+//Asignar disabled a los boyones
+function botones() {
+    btns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            btn.disabled = true;
+            verificarLetra(btn.textContent.toUpperCase());
+        });
+    });
 }
 
-function obtenerLetras(rta){
+
+function generarPreguntas() {
+    pregunta.innerHTML = pregRandom.pregunta;
+
+    let letras = obtenerLetras(rtaRandom);
+
+    for (let i = 0; i < rtaRandom.length; i++) {
+        let rayaLi = document.createElement('li');
+        rayaLi.textContent = "_";
+        rayaContainer.append(rayaLi);
+
+        //  rayaLi.textContent = letras[i];
+
+    }
+
+
+}
+
+function obtenerLetras(rta) {
     var letras = [];
-    for (var i = 0; i < rta.length; i++){
+    for (var i = 0; i < rta.length; i++) {
         letras.push(rta[i]);
     }
     return letras;
 }
 
 
-img.src = 'media/1.svg';
-img.src = 'media/2.svg';
-img.src = 'media/3.svg';
-img.src = 'media/4.svg';
-img.src = 'media/5.svg';
-img.src = 'media/6.svg';
-
 botones();
 generarPreguntas();
+
+function verificarLetra(letra) {
+    letra.toUpperCase;
+    if (rtaRandom.includes(letra)) {
+        console.log("Esta la letra!")
+    }
+
+    if (!rtaRandom.includes(letra)) {
+        error.textContent++;
+        console.log(error.textContent);
+        console.log("NO Esta la letra!")
+        img.src = imagenes[error.textContent];
+
+        //Si perdio..
+        if (error.textContent > 6) {
+            alert("perdite!");
+        }
+    }
+
+}
+
